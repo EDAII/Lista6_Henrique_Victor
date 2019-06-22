515973b30b855a1f0aed026d138802dfddebdcb4
Henrike100 <henrikem100@gmail.com>
2019-06-22 17:00:24 -0300

    Adicionando dados

--- src/especie.py
+++ src/especie.py
@@ -2,7 +2,7 @@ class Especie:
 
     __slots__ = ['nome', 'filo', 'classe', 'ordem']
 
-    def __init__(self, nome):
+    def __init__(self, nome, filo, classe, ordem):
         self.nome = nome
         self.filo = filo
         self.classe = classe
