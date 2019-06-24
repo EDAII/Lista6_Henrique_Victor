cb796d483c047add2917037a5bd3958c4c8b0794
Henrike100 <henrikem100@gmail.com>
2019-06-24 02:43:32 -0300

    Adição da Estrutura do Grafo

--- src/funcoes.py
+++ src/funcoes.py
@@ -1,187 +1,46 @@
 import os
 import random
-import string
 import time
 import matplotlib
 import matplotlib.pyplot as plt
-from paciente import Paciente
-from ordenacoes import *
+from graph import Graph
+from especie import Especie
+from dados import *
 import plotly
 import plotly.graph_objs as go
 import numpy as np
 import multiprocessing as mp
 
-maiuscula = string.ascii_uppercase
-minuscula = string.ascii_lowercase
-potencia_maxima = 10
-quant_process = 8
+quant_process = 4
 
-def gerar_pacientes_aleatorios(tamanho):
+def gerar_grafo_aleatorio(v, e):
+    grafo = Graph()
     pool = mp.Pool(processes=quant_process)
-    fila = pool.map(gerar_pacientes_aleatorios_aux, range(0, tamanho))
+    lista = pool.map(gerar_grafo_aleatorio_aux, range(0, v))
 
-    return fila
+    if(e == v*v-v):
+        for a in range(v):
+            for b in range(v):
+                grafo.add_edge(lista[a], lista[b])
+    else:
+        i = 0
+        while (i != e):
+            a, b = random.randrange(0, len(lista)), random.randrange(0, len(lista))
+            if a == b:
+                continue
 
+            if (grafo.add_edge(lista[a], lista[b]) == False):
+                continue
 
-def gerar_pacientes_aleatorios_aux(i):
-    nome = ''.join([random.choice(maiuscula)])
-    nome += ''.join(random.choice(minuscula) for _ in range(random.randrange(3, 10)))
-
-    sexo = ''.join([random.choice("MF")])
-
-    idade = random.randrange(0, 121)
-
-    gravidade = random.randrange(1, 6)
-
-    return Paciente(nome, sexo, idade, gravidade, i+1)
-
-def paciente_unico(fila, nome, sexo, idade, gravidade):
-    fila.append(Paciente(nome, sexo, idade, gravidade, len(fila) + 1))
-
-
-def mostrar_fila(fila, tamanho):
-    nomes = []
-    sexos = []
-    idades = []
-    gravidades = []
-    ordemChegadas = []
-
-    for i in range(tamanho):
-        nomes.append(fila[i].nome)
-        sexos.append(fila[i].sexo)
-        idades.append(fila[i].idade)
-        gravidades.append(fila[i].gravidade)
-        ordemChegadas.append(fila[i].ordemChegada)
-
-    indices = ['<b>NOMES</b>', '<b>SEXOS</b>', '<b>IDADES</b>',
-               '<b>GRAVIDADE</b>', '<b>ORDEM DE CHEGADA</b>']
-    trace = go.Table(
-        header=dict(
-            values=indices,
-            line=dict(color='#000'),
-            fill=dict(color='blue'),
-            font=dict(color='#fff', size=20)
-        ),
-        cells=dict(
-            values=[nomes, sexos, idades, gravidades, ordemChegadas],
-            font=dict(color='#000', size=14),
-            fill=dict(color='#F1F8FB'),
-            height=25
-        )
-    )
-
-    data = [trace]
-    plotly.offline.plot(data, filename='fila.html')
-
-
-def comparar_ordenacoes(fila, desordenado):
-    lista_tempos = {}
-
-    # Heap Sort Recursivo - HSR
-    fila = desordenado.copy()
-    inicio = time.perf_counter()
-    merge_sort(fila)
-    fim = time.perf_counter()
-    lista_tempos['HSR'] = (fim - inicio)
-
-    # Heap Sort Interativo - HSI
-    fila = desordenado.copy()
-    inicio = time.perf_counter()
-    merge_sort(fila)
-    fim = time.perf_counter()
-    lista_tempos['HSI'] = (fim - inicio)
-
-    tipos = ['Heap Sort Recursivo', 'Heap Sort Interativo']
-    tempos = [lista_tempos['HSR'], lista_tempos['HSI']]
-
-    _, ax = plt.subplots(figsize=(16, 9))
-    ax.set(xlabel='Metodo de Ordenacao', ylabel='Tempo (s)')
-    plt.figure(1)
-    plt.bar(tipos, tempos)
-
-    for i, v in enumerate(tempos):
-        plt.text(i-0.4, max(tempos)/100, " "+str(v), color='black',
-                 va='center', fontweight='bold', fontsize=12)
-
-    plt.suptitle(
-        'Tempo em segundos para ordenar {} pacientes'.format(len(registros)))
-    plt.show()
-
-
-def ord_HSR(vetor):
-    inicio = time.perf_counter()
-    heap_sort_recursivo(vetor)
-    fim = time.perf_counter()
-
-    return (fim-inicio)
-
-
-def ord_HSI(vetor):
-    inicio = time.perf_counter()
-    heap_sort_recursivo(vetor)
-    fim = time.perf_counter()
-
-    return (fim-inicio)
-
-
-def comparacoes():
-    desordenado = []
-    reg_HSR = []
-    reg_HSI = []
-
-    for i in range(potencia_maxima):
-        reg_HSR.append([])
-        reg_HSI.append([])
+            i += 1
     
-    for i in range(potencia_maxima):
-        desordenado = gerar_pacientes_aleatorios(2**(i+1))
-
-        reg_HSR[i] = desordenado.copy()
-        reg_HSI[i] = desordenado.copy()
-    
-    pool = mp.Pool(processes=quant_process)
-
-    tempo_HSR = pool.map(gerar_pacientes_aleatorios_aux, range(0, tamanho))
-    tempo_HSI = pool.map(gerar_pacientes_aleatorios_aux, range(0, tamanho))
-
-def printar_grafico(HSR, HSI):
-    x = np.array([])
-    for i in range(potencia_maxima):
-        z = 2**(i+1)
-        x = np.append(x, z)
-
-    t = x
-
-    fig, ax = plt.subplots()
-    ax.set_title('Comparação entre os Algoritmos')
-    ax.set(xlabel='Quantidade de elementos', ylabel='Tempo (s)')
-    line1, = ax.plot(t, merge, lw=2, color='red', label='Merge Sort')
-    line2, = ax.plot(t, bucket, lw=2, color='blue', label='BucketSort')
-    leg = ax.legend(loc='upper left', fancybox=True, shadow=True)
-    leg.get_frame().set_alpha(0.4)
-
-    lines = [line1, line2]
-    lined = dict()
-    for legline, origline in zip(leg.get_lines(), lines):
-        legline.set_picker(5)  # 5 pts tolerance
-        lined[legline] = origline
+    return grafo
 
 
-    def onpick(event):
-        # on the pick event, find the orig line corresponding to the
-        # legend proxy line, and toggle the visibility
-        legline = event.artist
-        origline = lined[legline]
-        vis = not origline.get_visible()
-        origline.set_visible(vis)
-        # Change the alpha on the line in the legend so we can see what lines
-        # have been toggled
-        if vis:
-            legline.set_alpha(1.0)
-        else:
-            legline.set_alpha(0.2)
-        fig.canvas.draw()
+def gerar_grafo_aleatorio_aux(i):
+    nome = animais[random.randrange(0, len(animais))]
+    filo = filos[random.randrange(0, len(filos))]
+    classe = classes[random.randrange(0, len(classes))]
+    ordem = ordens[random.randrange(0, len(ordens))]
 
-    fig.canvas.mpl_connect('pick_event', onpick)
-
-    plt.show()
+    return Especie(nome, filo, classe, ordem)
---
+++ src/graph.py
@@ -0,0 +1,46 @@
+class Graph(object):
+
+    __slots__ = ['__graph_dict']
+
+    def __init__(self, graph_dict=None):
+        """ initializes a graph object 
+            If no dictionary or None is given, 
+            an empty dictionary will be used
+        """
+        if graph_dict == None:
+            graph_dict = {}
+        self.__graph_dict = graph_dict
+
+    def vertices(self):
+        """ returns the vertices of a graph """
+        return list(self.__graph_dict.keys())
+
+    def add_vertex(self, vertex):
+        """ If the vertex "vertex" is not in 
+            self.__graph_dict, a key "vertex" with an empty
+            list as a value is added to the dictionary. 
+            Otherwise nothing has to be done. 
+        """
+        if vertex not in self.__graph_dict:
+            self.__graph_dict[vertex] = []
+        else:
+            return False
+
+    def add_edge(self, vertex1, vertex2):
+        """ assumes that edge is of type set, tuple or list; 
+            between two vertices can be multiple edges! 
+        """
+        if vertex1 in self.__graph_dict:
+            if vertex2 not in self.__graph_dict[vertex1]:
+                self.__graph_dict[vertex1].append(vertex2)
+            else:
+                return False
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
+        
+        return True
+
+"""
+graph.add_vertex("z")
+graph.add_edge({"a","z"})
+"""
